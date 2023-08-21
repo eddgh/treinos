22b748bb37bc4694b25cc5a210acf7ef2a7b2055
Edmundo Jose Batista <eddmundosjb@gmail.com>
2023-08-20 23:04:11 -0300

    feat: iniciando o projeto

---
+++ treino01/src/components/LandingPage/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import styles from "./css/main.module.css";
+export default function LandingPage() {
+  return (
+    <>
+      <div id={styles.pageLanding}>
+        <div id={styles.container}>
+          <header>
+            <img
+              id={styles.logo}
+              src="./public/images/shoppingcart_77968.svg"
+              height="90"
+              alt="logomarca de Happy"
+            />
+
+            <div className={styles.title}>
+              <strong>Meu Mercado</strong>
+              <p>Monte a sua lista de compras</p>
+            </div>
+          </header>
+
+          <main>
+            <h1>Bem-vindo ao Meu Mercado!</h1>
+
+            <section className={styles.adding}>
+              <p>Adicione um item abaixo e clique na seta</p>
+
+              <a href="#" title="Adicionar item">
+                <img
+                  src="./public/images/pngwing.com.svg"
+                  height="20"
+                  alt="imagem da seta 'dicionar item'"
+                />
+              </a>
+            </section>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
