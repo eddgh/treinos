2babc60f5b042735123a9e45e9aa4e9161b03afc
EDMUNDO JOSE BATISTA <72671378+eddgh@users.noreply.github.com>
2023-08-21 18:30:37 -0300

    imagem do carrinho em 1rem

--- treino01/src/components/LandingPage/index.jsx
+++ treino01/src/components/LandingPage/index.jsx
@@ -9,7 +9,7 @@ export default function LandingPage() {
             <img
               id={styles.logo}
               src="/images/shoppingcart_77968.svg"
-              height="50%"
+              height="1rem"
               alt="logomarca de Happy"
             />
 
