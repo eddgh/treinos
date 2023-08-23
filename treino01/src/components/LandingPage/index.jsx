def4b62e600dfee66ef841d09b106fe1a61bf833
Edmundo Jose Batista <eddmundosjb@gmail.com>
2023-08-23 00:50:39 -0300

    inserido animações

--- treino01/src/components/LandingPage/index.jsx
+++ treino01/src/components/LandingPage/index.jsx
@@ -6,25 +6,25 @@ export default function LandingPage() {
       <div id={styles.pageLanding}>
         <div id={styles.container}>
           <header>
-            <img
+            <img className={styles.animateup}
               id={styles.logo}
               src="/images/shoppingcart_77968.svg"
               alt="logomarca de Meu Mercado"
             />
 
-            <div className={styles.title}>
+            <div className={`${styles.title} ${styles.animateup}`}>
               <strong>Meu Mercado</strong>
               <p>Monte a sua lista de compras</p>
             </div>
           </header>
 
           <main>
-            <h1>Bem-vindo ao Meu Mercado!</h1>
+            <h1 className={styles.animateup}>Bem-vindo ao Meu Mercado!</h1>
 
             <section className={styles.adding}>
-              <p>Adicione um item abaixo e clique na seta</p>
+              <p className={styles.animateup}>Adicione um item abaixo e clique na seta</p>
 
-              <a href="#" title="Adicionar item">
+              <a href="#" title="Adicionar item" className={styles.animateup}>
                 <img
                   src="/images/pngwing.com.png"
                   alt="imagem da seta 'adicionar item'"
